23b8467fc7495ded3f7360806882f5d1d9b2368a
K0garash1 <brayanarellano1605@gmail.com>
2021-10-04 14:43:45 -0500

    Sistema de sesion
    añadidos métodos de registro, inicio y cerrado de sesión de usuarios

--- Database.py
+++ Database.py
@@ -1,4 +1,4 @@
-import psycopg2
+import psycopg2, uuid
 
 db_user = 'postgres'
 db_password = 'hyperpro16'
@@ -80,6 +80,54 @@ def select_song(song):
         data = cur.fetchone()
         cur.close()
         return data
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def select_user_by_email(email):
+    try:
+        conn = psycopg2.connect(
+            f"dbname=Perfect_Rithm user={db_user} password={db_password}")
+        cur = conn.cursor()
+        cur.execute(
+            f"SELECT * FROM usuario U WHERE U.email = '{email}'")
+        data = cur.fetchone()
+        cur.close()
+        return data
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def select_user_by_id(uid):
+    try:
+        conn = psycopg2.connect(
+            f"dbname=Perfect_Rithm user={db_user} password={db_password}")
+        cur = conn.cursor()
+        cur.execute(
+            f"SELECT * FROM usuario U WHERE U.uid = '{uid}'")
+        data = cur.fetchone()
+        cur.close()
+        return data
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def reg_user(username,email,password):
+    uid = str(uuid.uuid5(uuid.NAMESPACE_DNS, username))
+    try:
+        conn = psycopg2.connect(
+            f"dbname=Perfect_Rithm user={db_user} password={db_password}")
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO usuario (uid, username, email, password, level, user_score) VALUES (%s,%s,%s,%s,%s,%s)", (uid,username,email,password,1,0))
+        conn.commit()
+        cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, url_for, request, render_template, session
+from flask import Flask, redirect, url_for, request, render_template, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 import Database
 
 app = Flask(__name__)
@@ -43,12 +44,52 @@ def leaderboard():
 
 @app.route("/profile")
 def profile():
-    return render_template('profile.html')
+    if 'loggedin' in session:
+        data = Database.select_user_by_id(session['id'])
+        return render_template('profile.html',data=data)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    if request.method == 'GET':
-        return render_template("login.html")
     if request.method == 'POST':
-        return redirect(url_for("index"))
+        user_email = request.form['login_email']
+        password = request.form['login_password']
+        user = Database.select_user_by_email(user_email)
+        print(user)
+        if user:
+            password_hash = user[3]
+            print(generate_password_hash(password))
+            print(password_hash)
+            if check_password_hash(password_hash,password):
+                session['loggedin']=True
+                session['id']=user[0]
+                session['username']=user[1]
+                return redirect(url_for('index'))
+            else:
+                flash('Incorrect username/password')
+        else:
+            # Account doesnt exist or username/password incorrect
+            flash('Incorrect username/password')
+    return render_template('login.html')
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method=='POST':
+        username = request.form['reg_name']
+        email = request.form['reg_email']
+        password = request.form['reg_password']
+        Database.reg_user(username,email,generate_password_hash(password,"sha256"))
+        return redirect(url_for('login'))
+    else:
+        return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
