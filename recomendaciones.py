dda8681e161519726160a5eb54b548b02b0e2799
JoRgEXx1899 <daniel990918@gmail.com>
2021-10-11 22:08:48 -0500

    Update recomendaciones.py

--- recomendaciones.py
+++ recomendaciones.py
@@ -1,5 +1,5 @@
 from pyswip import Prolog
 prolog = solution.Prolog()
 list(prolog.query("getArtistTracks(X,TrackIds,TrackNames)")) == [{'X': 'Adele'}, {'X': 'Sam Smith'}]
-for soln in prolog.query("father(X,Y,Z)"):
+for soln in prolog.query("getArtistTracks(X,Y,Z)"):
     print(soln["X"], "is the artist of", soln["Z"])
