b47a1e8e5049ec22ddcd45159e214f9062c124e9
vh2346 <51457629+vh2346@users.noreply.github.com>
2021-10-12 12:51:07 -0500

    cambios sistema

--- recomendaciones.py
+++ recomendaciones.py
@@ -1,8 +1,6 @@
-from pyswip import Prolog
-prolog = solution.Prolog()
-list(prolog.query("getArtistTracks(X,TrackIds,TrackNames)")) == [{'X': 'Adele'}, {'X': 'Sam Smith'}]
-for soln in prolog.query("getArtistTracks(X,Y,Z)"):
-    print(soln["X"], "is the artist of", soln["Z"])
-def public recomendado(artist){
-	print(list(prolog.query("getArtistTracks(X,TrackIds,TrackNames)")) == [{'X': artist}]);
-}
+from  pyswip import Prolog
+prolog=Prolog()
+prolog.consult("solution.pl")
+g= prolog.query("getArtistTracks(_,_,TrackNames)")
+L=list(g)
+print(L)
