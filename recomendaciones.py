1cafce1f07da1c1076a2b4d751aab2684b73f74a
JoRgEXx1899 <daniel990918@gmail.com>
2021-10-11 22:16:44 -0500

    Update recomendaciones.py

--- recomendaciones.py
+++ recomendaciones.py
@@ -3,3 +3,6 @@ prolog = solution.Prolog()
 list(prolog.query("getArtistTracks(X,TrackIds,TrackNames)")) == [{'X': 'Adele'}, {'X': 'Sam Smith'}]
 for soln in prolog.query("getArtistTracks(X,Y,Z)"):
     print(soln["X"], "is the artist of", soln["Z"])
+def public recomendado(artist){
+	print(list(prolog.query("getArtistTracks(X,TrackIds,TrackNames)")) == [{'X': artist}]);
+}
